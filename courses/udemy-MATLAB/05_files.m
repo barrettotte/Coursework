7d503d8d228998d9109bae32c164b6204b352439
Barrett <barrettotte@gmail.com>
2020-11-04 21:07:44 -0500

    remove udemy courses

--- courses/udemy-MATLAB/05_files.m
+++
@@ -1,7 +0,0 @@
-%% File/Directory Information
-
-everything = dir;                                     % info about all files/directories
-directories = {everything([everything.isdir]).name};  % get all directory names
-
-stuff = {dir('*variab*').name};  % wildcard search
-nameLens = cellfun(@length, directories);
--- courses/udemy-MATLAB/README.md
+++
@@ -1,9 +0,0 @@
-# Udemy - Master MATLAB through Guided Problem Solving
-
-https://www.udemy.com/course/master-matlab-through-guided-problem-solving/learn/
-
-An introduction to MATLAB.
-
-<br>
-
-**No coursework, just following along with examples**
